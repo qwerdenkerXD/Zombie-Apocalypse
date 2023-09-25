ffd31e603d9ca5b6a9a008d90f0e8460ac00b272
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-09-25 11:48:32 +0200

    added idea of simulate_with_zombies_no_heroes

--- simulation.py
+++ simulation.py
@@ -6,41 +6,100 @@ HMN_CITIZENS = 9000
 HERO_CITIZENS = 1000
 
 SPECIES = {
-    "HUMANS": HMN_CITIZENS,  # common humans without any skills
+    "HUMANS": 0,  # common humans without any skills
 
-    "HEROES": HERO_CITIZENS,  # special humans, zombie killer
+    "HEROES": 0,  # special humans, zombie killer
 
     "DEADS": 0,  # just dead matter, no living-like thing
 
     "ZOMBIES": 0  # undead
 }
 
-DAY = 0
-
 
 def main():
     random.seed(1)
     duration = 3650
     simulate_normal(duration)
-    simulate(duration)
-
-
-def simulate(iterations: int):
-    global DAY
-    import random
-    SPECIES["ZOMBIES"] = 10
+    simulate_with_zombies_no_heroes(duration)
 
 
 def simulate_normal(iterations: int):
-    global DAY
-    import random
+    """
+        The simulation without zombies -> blank
+    """
+    SPECIES["HUMANS"] = HMN_CITIZENS
+    SPECIES["HEROES"] = HERO_CITIZENS
+    SPECIES["DEADS"] = 0
+    SPECIES["ZOMBIES"] = 0
+
     change = {"HUMANS": [], "HEROES": [], "DEADS": [], "ZOMBIES": []}
-    for i in range(iterations):
+    for day in range(iterations):
         population_growth()
-        DAY += 1
         for i in change:
             change[i].append(SPECIES[i])
     plot(change, "normal.png")
+
+
+def population_growth():
+    """
+        The daily growth of the population, independent to zombies
+
+        let's take city Bramsche with 10k citizens
+        https://www.deutschland123.de/bramsche-geburten -> 260 / a
+        https://www.deutschland123.de/bramsche-todesfaelle -> 301 / a -> 3.01% / a
+        https://www.deutschland123.de/bramsche-umz%C3%BCge-zu-und-fortz%C3%BCge -> (1779 - 1815) / a
+    """
+    growth = (260 + 1779 - 1815) / 365
+    SPECIES["DEADS"] += .0301 * (SPECIES["HUMANS"] + SPECIES["HEROES"]) / 365
+    SPECIES["HUMANS"] += growth / 2 * bool(SPECIES["HUMANS"])
+    SPECIES["HEROES"] += growth / 2 * bool(SPECIES["HEROES"])
+
+
+def simulate_with_zombies_no_heroes(iterations: int):
+    """
+        The simulation without heroes, so if all citizens were normal guys in a city with some zombies.
+    """
+    SPECIES["HUMANS"] = HMN_CITIZENS + HERO_CITIZENS
+    SPECIES["HEROES"] = 0
+    SPECIES["DEADS"] = 0
+    SPECIES["ZOMBIES"] = 10
+
+    change = {"HUMANS": [], "HEROES": [], "DEADS": [], "ZOMBIES": []}
+    tasks = [human_kills_zombie, zombie_transforms_human, human_kills_human]
+    for i in range(iterations):
+        population_growth()
+        for day in range(iterations):
+            random.shuffle(tasks)
+            for task in tasks:
+                task()
+
+        for i in change:
+            change[i].append(SPECIES[i])
+    plot(change, "zombified_no_heroes.png")
+
+
+def human_kills_zombie():
+    """
+        The change of zombies killed by humans.
+        This gets higher the more zombies exist.
+    """
+    pass
+
+
+def zombie_transforms_human():
+    """
+        The change of humans turned into zombies.
+        This gets higher the more zombies exist.
+    """
+    pass
+
+
+def human_kills_human():
+    """
+        The change of humans killed by humans.
+        This gets higher everyday because of the stress that changes the humans' minds.
+    """
+    pass
 
 
 def plot(species: dict, file: str):
@@ -50,18 +109,6 @@ def plot(species: dict, file: str):
     plt.legend(loc="upper left")
     plt.savefig(file)
     plt.clf()
-
-
-def population_growth():
-    """
-        let's take city Bramsche with 10k citizens
-        https://www.deutschland123.de/bramsche-geburten -> 260 / a
-        https://www.deutschland123.de/bramsche-todesfaelle -> -301
-        https://www.deutschland123.de/bramsche-umz%C3%BCge-zu-und-fortz%C3%BCge -> 1779 - 1815
-    """
-    growth = (260 - 301 + 1779 - 1815) / 365
-    SPECIES["HUMANS"] += growth / 2
-    SPECIES["HEROES"] += growth / 2
 
 
 if __name__ == '__main__':
