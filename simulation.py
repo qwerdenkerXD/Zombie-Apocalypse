de9d66df8e0e694c08f8f0474fce1f5e85d573e7
107982913+qwerdenkerXD <107982913+qwerdenkerXD@users.noreply.github.com>
2023-09-25 12:56:09 +0200

    implemented human_kills_zombie

--- simulation.py
+++ simulation.py
@@ -15,6 +15,8 @@ SPECIES = {
     "ZOMBIES": 0  # undead
 }
 
+DAY = 0
+
 
 def main():
     random.seed(1)
@@ -59,6 +61,7 @@ def simulate_with_zombies_no_heroes(iterations: int):
     """
         The simulation without heroes, so if all citizens were normal guys in a city with some zombies.
     """
+    global DAY
     SPECIES["HUMANS"] = HMN_CITIZENS + HERO_CITIZENS
     SPECIES["HEROES"] = 0
     SPECIES["DEADS"] = 0
@@ -66,24 +69,36 @@ def simulate_with_zombies_no_heroes(iterations: int):
 
     change = {"HUMANS": [], "HEROES": [], "DEADS": [], "ZOMBIES": []}
     tasks = [human_kills_zombie, zombie_transforms_human, human_kills_human]
-    for i in range(iterations):
+    for day in range(iterations):
         population_growth()
-        for day in range(iterations):
-            random.shuffle(tasks)
-            for task in tasks:
-                task()
+        DAY = day
+        random.shuffle(tasks)
+        for task in tasks:
+            task()
 
         for i in change:
             change[i].append(SPECIES[i])
+    print(change["ZOMBIES"][-1])
     plot(change, "zombified_no_heroes.png")
 
 
 def human_kills_zombie():
     """
         The change of zombies killed by humans.
-        This gets higher the more zombies exist.
+        One human has the skill to kill one of ten zombies if he has no choice.
+        So if there were ten zombies and two humans, they would kill one only still because one of them can fly.
+        If there were 20 zombies and two humans, they would kill two of them.
+        And if there are less then ten, no zombie is gonna be killed.
+        In short, the zombies reduce by a tenth if they are less than the tenfold of the humans, else by the number of humans.
+
+        In the beginning they hesitate with killing because of moral issues.
     """
-    pass
+    current_population = SPECIES["HUMANS"] + SPECIES["HEROES"]
+    hesitation_factor = sigmoid(SPECIES["ZOMBIES"], current_population / 10)
+    if int(SPECIES["ZOMBIES"]) < 10 * int(current_population):
+        SPECIES["ZOMBIES"] -= hesitation_factor * (SPECIES["ZOMBIES"] // 10)
+    else:
+        SPECIES["ZOMBIES"] -= hesitation_factor * (SPECIES["HUMANS"])
 
 
 def zombie_transforms_human():
@@ -100,6 +115,15 @@ def human_kills_human():
         This gets higher everyday because of the stress that changes the humans' minds.
     """
     pass
+
+
+def sigmoid(x: float, where_to_be_one: float) -> float:
+    """
+        The sigmoid function, converges to 1.
+        The normal function with e^-x, so factor 1 for x is close to 1 at x=6 -> factor=6/(x where it should be close 1)
+    """
+    from math import exp
+    return 1 / (1 + exp(-(6 / (where_to_be_one / 2)) * (x - (where_to_be_one / 2))))
 
 
 def plot(species: dict, file: str):
